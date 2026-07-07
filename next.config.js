27af626137422fd64029cd5bc31f80bd08dda916
Luky97 <102862398+Luky97@users.noreply.github.com>
2026-07-07 20:02:44 +0300

    Update Next.js configuration settings

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/over-deriv',
+  images: {
+    unoptimized: true,
+  },
   transpilePackages: ['@deriv/core'],
 }
 
